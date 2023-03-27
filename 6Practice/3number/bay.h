b202dbaa03765388b1b7d00d32d4ebedc75b9986
Archei1 <125820933+Archei1@users.noreply.github.com>
2023-03-27 14:38:19 +0300

    Create bay.h

---
+++ 6Practice/3number/bay.h
@@ -0,0 +1,15 @@
+#ifndef MIREACPPPRACTIC_BAY_H
+#define MIREACPPPRACTIC_BAY_H
+#include "sea.h"
+#include <string>
+
+class Bay : public Sea {
+protected:
+    string nearby;
+public:
+    Bay();
+    Bay(string name, string location, float size, float depth, string nearby, string type);
+    virtual void showInfo();
+};
+
+#endif
